66bee2fd3669f4d8ca163ee700441682f1ddd0c8
ajudge1001 <irontakeout@gmail.com>
2023-03-14 15:43:25 +0000

    added different jumping
    NOTE: feels a little "floaty"
    jump force and jump time may have to do with this
    
    or could j be like a global "gravity "value idk

--- Assets/Metroidvania/PlayerJump.cs
+++ Assets/Metroidvania/PlayerJump.cs
@@ -4,31 +4,68 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    [Header ("Public Vars")]
+    [Header ("Jump Details")]
     public float jumpForce;
-    public bool grounded;
-    private Rigidbody2D rb;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
 
 
-    [Header("Private Vars")]
+    [Header("Ground Details")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float radOCircle;
     [SerializeField] private LayerMask whatIsGround;
+     public bool grounded;
+
+
+    [Header("Components")]
+    private Rigidbody2D rb;
+
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
 
     }
     private void Update()
     {
+        // what it means to be grounded
         grounded = Physics2D.OverlapCircle(groundCheck.position,radOCircle,whatIsGround);
+
+        if(grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
         // use space and w to jump
         if(Input.GetButtonDown("Jump") && grounded)
         {
+            // jump!
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJumping = false;
 
         }
+
+        // to keep jumping while button is active 
+        if(Input.GetButton("Jump") && !stoppedJumping && (jumpTimeCounter > 0) )
+        {
+            
+            // jump
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        // if jump button is released
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
+        }
+
+
     }
 
      private void OnDrawGizmos() {
